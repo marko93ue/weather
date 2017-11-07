084d968f203a0fb590439f746ea615cd9e5aacc5
marko93ue <marko93ue@gmail.com>
2017-11-08 00:52:21 +0100

    Fixed some bugs

--- components/forecast.js
+++ components/forecast.js
@@ -3,15 +3,17 @@ angular.module("app").component('forecast',{
   template: 
 "<h1>{{$ctrl.forecast.city.name + ', ' +$ctrl.forecast.city.country}}</h1>"+
 "<span ng-if='$ctrl.forecast.error'> {{$ctrl.forecast.error}}</span>"+
-"<div class='row'>"+
-"<div ng-repeat='day in days' class='col'>"+
-"  <h6 >{{day}}</h6>"+
-"  <div ng-repeat='item in $parent.$ctrl.forecast.list' >"+
-"    <span ng-if='item.day === day' >"+
-"      {{toUnixDate(item.dt_txt) | date: 'hh a'}} {{(item.main.temp-273).toFixed()}} {{item.weather.description}}C"+
-"    </span>"+
+"<div class='row align-items-start'>"+
+"  <div ng-repeat='day in days' >"+
+"    <div class='col'>"+
+"      <h6 >{{day}}</h6>"+
+"      <div ng-repeat='item in $parent.$ctrl.forecast.list'  >"+
+"        <span ng-if='item.day === day' >"+
+"          {{toUnixDate(item.dt_txt) | date: 'hh a'}} {{(item.main.temp-273).toFixed()}} {{item.weather.description}}C"+
+"        </span>"+
+"      </div>"+
+"    </div>"+
 "  </div>"+
-"</div>"+
 "</div>"+
 "<button ui-sref='home'>Close</button>",
 
@@ -23,12 +25,12 @@ angular.module("app").component('forecast',{
       //console.log(newValue);
       if(newValue)
         $scope.parseDays(newValue);
-      
+ 
     });
     
     $scope.toUnixDate = function( jsonDate)
     {
-      return ( new Date(jsonDate).getTime());
+      return ( new Date(jsonDate.split("-").join("/")).getTime());
     }
     
      $scope.parseDays = function (list)
