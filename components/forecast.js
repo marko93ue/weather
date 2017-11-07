78c99e2acee641f3daae31e17502f3c1ada1f200
marko93ue <marko93ue@gmail.com>
2017-11-07 22:55:00 +0100

    Improved 5 day forecast

--- components/forecast.js
+++ components/forecast.js
@@ -1,20 +1,49 @@
 angular.module("app").component('forecast',{
   bindings: { forecast: '<'},
   template: 
-"<h1>{{$ctrl.forecast.city.name}},{{$ctrl.forecast.city.country}}</h1>"+
+"<h1>{{$ctrl.forecast.city.name + ', ' +$ctrl.forecast.city.country}}</h1>"+
 "<span ng-if='$ctrl.forecast.error'> {{$ctrl.forecast.error}}</span>"+
-"<div ng-repeat='item in $ctrl.forecast.list'>"+
-"  <span>{{ toUnixDate(item.dt_txt) | date:'MMM, d hh a'  }} {{(item.main.temp-273).toFixed()}}C {{item.weather[0].description}}</span>"+
+"<div class='row'>"+
+"<div ng-repeat='day in days' class='col'>"+
+"  <h6 >{{day}}</h6>"+
+"  <div ng-repeat='item in $parent.$ctrl.forecast.list' >"+
+"    <span ng-if='item.day === day' >"+
+"      {{toUnixDate(item.dt_txt) | date: 'hh a'}} {{(item.main.temp-273).toFixed()}} {{item.weather.description}}C"+
+"    </span>"+
+"  </div>"+
+"</div>"+
 "</div>"+
 "<button ui-sref='home'>Close</button>",
 
-  controller: function($scope){
+  controller: function($scope,$filter){
+    
+    $scope.$ctrl.forecast = {};
+    $scope.days = [];
+    $scope.$watchCollection('$ctrl.forecast.list', function(newValue, oldValue){
+      //console.log(newValue);
+      if(newValue)
+        $scope.parseDays(newValue);
+      
+    });
     
     $scope.toUnixDate = function( jsonDate)
     {
       return ( new Date(jsonDate).getTime());
     }
-    console.log($scope);
+    
+     $scope.parseDays = function (list)
+    {
+      $scope.days = [];
+      for(let i = 0; i < list.length; i++)
+      {
+        var currentDay =  $filter('date')($scope.toUnixDate(list[i].dt_txt),'EEEE');
+        list[i].day = currentDay;
+        if($scope.days.indexOf(currentDay) < 0)
+          $scope.days.push(currentDay);
+      }
+      
+    }
+ 
   }
   
  
--- components/home.js
+++ components/home.js
@@ -2,29 +2,31 @@ angular.module("app").component('home',{
   bindings: { forecast: '<'},
   template: 
   
-'      <div class="row mt-5"  >'+
-'      <div  class="col-8" >'+
-'        <input type="button" ng-click="clearAll()" class="float-right" value="Clear All" />'+
-'        <br><br>'+
-'        <div ng-repeat="x in zipCodes" >'+       
-'          <div ng-if="x.show" class="alert alert-danger" >'+
-'            <span>Zip code: </span>'+
-'            <input type="text" ng-model="x.value" />'+
-'            <span ng-if="x.weather.dt" ui-sref="home.forecast({zipCode: x.value })" >'+
-'              {{x.weather.name}} {{ (x.weather.main.temp-273).toFixed()}}C {{x.weather.weather[0].description}}'+
-'            </span>'+
-'            <span ng-if="x.error">{{x.error}}</span>'+
-'          </div>'+          
-'        </div>'+
-'        <br>'+
-'        <input type="button" class="float-right" ng-if="zipCodes[0].show" ng-click="removeZipCode()"'+
-'          value="Remove zip last code" /> '+
-'        <input type="button" ng-click="addZipCode()" value="Add zip code" />'+
-'        <input type="button" ng-click="checkWeather()" value="Check current weather" />'+
-'        <br>'+        
+'  <div class="row mt-5"  >'+
+'    <div  class="col-10" >'+
+'      <input type="button" ng-click="clearAll()" class="float-right" value="Clear All" />'+
+'      <br><br>'+
+'      <div ng-repeat="x in zipCodes" >'+       
+'        <div ng-if="x.show" class="alert alert-danger" >'+
+'          <span>Zip code: </span>'+
+'          <input type="text" ng-model="x.value" />'+
+'          <span ng-if="x.weather.dt" ui-sref="home.forecast({zipCode: x.value })" >'+
+'            {{x.weather.name}} {{ (x.weather.main.temp-273).toFixed()}}C {{x.weather.weather[0].description}}'+
+'          </span>'+
+'          <span ng-if="x.error">{{x.error}}</span>'+
+'        </div>'+          
 '      </div>'+
-'      <ui-view></ui-view>'+
-'    </div>',
+'      <br>'+
+'      <input type="button" class="float-right" ng-if="zipCodes[0].show" ng-click="removeZipCode()"'+
+'        value="Remove zip last code" /> '+
+'      <input type="button" ng-click="addZipCode()" value="Add zip code" />'+
+'      <input type="button" ng-click="checkWeather()" value="Check current weather" />'+
+'      <br>'+        
+'    </div>'+
+'  </div>'+
+'  <div class="row">'+
+'    <ui-view></ui-view>'+
+'  </div>',
             
   controller: function($scope, $state, WeatherService){
   $scope.maxSize = 10;
@@ -103,7 +105,7 @@ angular.module("app").component('home',{
       $scope.zipCodes[i].error = undefined;
       
     }
-    $state.go('^', {}, {location: true});
+    $state.go('home');
   }
 }
   
